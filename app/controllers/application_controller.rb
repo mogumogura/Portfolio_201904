b895ce17d358bf7e6aafd20ee96caaebaa36f0fd
mogumogura <ankokagura@gmail.com>
2019-06-16 07:06:46 +0000

    Create sign_up,sign_in pege

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [
+        :name,
+        :postal_code,
+        :address])
+  end
+
+    # ログイン後に遷移するページ
+  def after_sign_in_path_for(resource)
+      user_path(resource)
+   end
+     # ログアウト後に遷移するページ
+  # def after_sign_out_path_for(resource_or_root)
+    # new_user_session_path
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,30 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+    redirect_to user_path(@user.id)
+    else
+    render :new
+    end
   end
 
   def edit
   end
 
   def update
+
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :postal_code, :address)
   end
 end
