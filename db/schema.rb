82eeb9547dba1935cc7d07ccdde7c1583f673392
mogumogura <ankokagura@gmail.com>
2019-06-27 10:32:08 +0000

    ruby version change7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_075150) do
+ActiveRecord::Schema.define(version: 2019_06_12_102230) do
 
   create_table "tourisms", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 2019_06_13_075150) do
     t.datetime "remember_created_at"
     t.string "postal_code", null: false
     t.string "address", null: false
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
