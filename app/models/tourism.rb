45f109f3f90b0c220006f7999d958b2a76604d0d
mogumogura <ankokagura@gmail.com>
2019-07-12 12:32:52 +0000

    delete home

--- app/models/tourism.rb
+++ app/models/tourism.rb
@@ -8,24 +8,14 @@ class Tourism < ApplicationRecord
   def self.search(search)
     word = search
     @data={
-      "keyid": "496e40bdc04b4f2a3187a01c2afabd6d",
+      "keyid": "8c21c9dbcd8841703fc79465ef68b9c1",
       "freeword": word,
-      "hit_per_page": 40,
+      "hit_per_page": 30,
     }
 
     query = @data.to_query
 
     json = Net::HTTP.get(URI.parse('https://api.gnavi.co.jp/RestSearchAPI/v3/?'+query))
     return JSON.parse(json) #配列形式に変換
-
-
-    #uri = Addressable::URI.parse('https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=496e40bdc04b4f2a3187a01c2afabd6d&address=&freeword=&hit_per_page=10')
-    # @hairetu = JSON.parse(json) #配列形式に変換
-    # binding.pry
-    # if search
-    #   where("freeword LIKE ?","%#{search}%")
-    # else
-    #   all
-    # end
   end
 end
